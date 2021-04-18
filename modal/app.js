88d10aa56494da9e979a33eafd3fba6d160e4888
Punhan Huseynov <husepunhan@gmail.com>
2021-04-18 07:44:06 +0400

    modal added

--- modal/app.js
+++ modal/app.js
@@ -7,12 +7,15 @@ for(let i=0;i<blogcard.length;i++){
       
         overlay.style.height='100%';
         overlay.style.opacity='100%';
+        overlay.firstElementChild.firstElementChild.nextElementSibling.style.height='100%';
         
         overlay.firstElementChild.firstElementChild.firstElementChild.src=blogcard[i].firstElementChild.firstElementChild.src
+        overlay.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.firstElementChild.innerHTML=blogcard[i].firstElementChild.nextElementSibling.firstElementChild.innerHTML
+        overlay.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.innerHTML=blogcard[i].firstElementChild.nextElementSibling.nextElementSibling.innerHTML
     })
 }
 closebutton.onclick=()=>{
-   
+    overlay.firstElementChild.firstElementChild.nextElementSibling.style.height='0%';
     overlay.style.height='0%';
     overlay.style.opacity='0%';
 }
